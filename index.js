ab32b4d6b7842d70662eed407e7a3986284294de
gfloresuth <dexterminio@gmail.com>
2016-11-16 16:00:24 -0700

    Primeros archivos

---
+++ index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+const PORT = 3000;
+app.use('/',express.static(__dirname + '/public'));
+app.listen(PORT, function(){
+    // Se muestra mensaje de exito
+    console.log('Servidor ejecutandose en http://localhost:'+PORT+"/");
+})
