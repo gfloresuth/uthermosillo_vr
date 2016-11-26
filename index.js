265cabda6737bb015049971afbceefc0d2595eda
gfloresuth <dexterminio@gmail.com>
2016-11-25 17:33:42 -0700

    Cambios

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 const PORT = 3000;
+
 app.use('/',express.static(__dirname + '/public'));
 app.listen(PORT, function(){
     // Se muestra mensaje de exito
